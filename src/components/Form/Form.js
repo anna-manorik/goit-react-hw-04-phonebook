503e764fe970f4a3cb883859d2e54b65b016766b
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-18 12:44:56 +0200

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import styles from './form.module.css';
 
 export default function Form({ onSubmit }) {
@@ -72,67 +73,6 @@ export default function Form({ onSubmit }) {
   );
 }
 
-// class Form extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleChange = e => {
-//     const { name, value } = e.currentTarget;
-
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-
-//     this.props.onSubmit(this.state);
-//     this.resetForm();
-
-//     e.currentTarget.elements.name.value = '';
-//     e.currentTarget.elements.number.value = '';
-//   };
-
-//   resetForm = () => {
-//     this.setState({ name: '', number: '' });
-//   };
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label>
-//           Name:
-//           <input
-//             className={styles.input}
-//             type="text"
-//             name="name"
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             onChange={this.handleChange}
-//             required
-//           />
-//         </label>
-//         <br />
-//         <label>
-//           Phone num:
-//           <input
-//             className={styles.input}
-//             type="tel"
-//             name="number"
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             onChange={this.handleChange}
-//             required
-//           />
-//         </label>
-//         <br />
-//         <button type="submit" className={styles.button}>
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
-
-// export default Form;
+Form.propTypes = {
+  onSubmit: PropTypes.func,
+};
